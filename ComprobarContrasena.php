b60bb15df52212715a3da0afed7b6b61b2295a28
Unknown <swg10ci@gmail.com>
2017-11-21 22:06:37 +0100

    Entrega

---
+++ ComprobarContrasena.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('lib/nusoap.php');
+require_once('lib/class.wsdlcache.php');
+
+$ns="https://iv27c.000webhostapp.com/Lab5/";
+$server = new soap_server;
+$server->configureWSDL('comprobar',$ns);
+$server->wsdl->schemaTargetNamespace=$ns;
+
+
+$server->register('comprobar',
+array('x'=>'xsd:string'),
+array('z'=>'xsd:string'),
+$ns);
+
+function comprobar ($pass){
+
+	$fich = file_get_contents("toppasswords.txt");
+	if (strpos($fich, $pass) == false) {
+        return 'VALIDA';
+    }
+    else{
+    	return 'INVALIDA';
+    }
+
+}
+
+if ( !isset( $HTTP_RAW_POST_DATA ) ) $HTTP_RAW_POST_DATA =file_get_contents( 'php://input' );
+
+$server->service($HTTP_RAW_POST_DATA);
+?>
